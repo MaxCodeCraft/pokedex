a6868f9d17d4a068ffe2a0712d8feca09633df35
MaxCodeCraft <maxcodecraft06@gmail.com>
2024-04-25 14:45:47 +0200

    finished

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,7 @@ function App({ Component, pageProps }) {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Pokedex</title>
+        <meta description="This is a super Pokedex, here you can fin all the pokemons to date !" />
         <link rel="icon" type="image/x-icon" href="/favicon.ico"></link>
       </Head>
       <Component {...pageProps} />
